a7c291e9a396eab0bb43f17f1a6b90cec6a4b48c
shinysatellite <sws092298@outlook.com>
2021-08-13 00:45:37 +0800

    fix the home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,28 +8,9 @@ export default function Home() {
     <Grid container spacing={2} justify="center" alignItems="center">
       <Grid item xs={12} sm={6}>
         <Typography component="h2" variant="h3">
-          Example To Do App
+          Enjoy All Cates
         </Typography>
-        <Box py={2} pr={4}>
-          <Typography>
-            This headless frontend was built with{' '}
-            <a href="https://material-ui.com/">Material UI</a> and scaffolded
-            with{' '}
-            <a href="https://reactjs.org/docs/create-a-new-react-app.html">
-              Create React App
-            </a>
-            . The project authenticates and stores data using{' '}
-            <a href="https://github.com/devinsays/laravel-react-bootstrap">
-              an API built in Laravel
-            </a>
-            .
-          </Typography>
-          <Typography>
-            <a href="https://github.com/devinsays/laravel-react-material">
-              Source code and documentation on GitHub.
-            </a>
-          </Typography>
-        </Box>
+        <p></p>
       </Grid>
       <Grid item xs={12} sm={6}>
         <LoginForm />
